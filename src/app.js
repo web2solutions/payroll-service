005da5c7b9b06b932503c0b1de12b0f670cd1853
José Eduardo Almeida <web2solucoes@gmail.com>
2023-12-19 11:00:48 -0300

    feat(GET /contracts): ability to list contracts per user

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ app.set('sequelize', sequelize)
 app.set('models', sequelize.models)
 
 
-app.get('/contracts/:id',getProfile ,async (req, res) =>{
+app.get('/contracts/:id', getProfile ,async (req, res) =>{
     const {Contract} = req.app.get('models')
     const {id} = req.params;
     const query = { where: {  id } }
@@ -20,6 +20,23 @@ app.get('/contracts/:id',getProfile ,async (req, res) =>{
     const contract = await Contract.findOne(query)
     if(!contract) return res.status(404).end()
     res.json(contract)
+});
+
+
+app.get('/contracts', getProfile ,async (req, res) =>{
+    const {Contract} = req.app.get('models')
+    // const {id} = req.params;
+    const query = { where: { status: 'in_progress' } }
+
+    if(req.profile.type === 'client') {
+        query.where.ClientId = req.profile.id
+    } else if(req.profile.type === 'contractor') {
+        query.where.ContractorId = req.profile.id
+    }
+
+    const contracts = await Contract.findAll(query)
+    
+    res.json(contracts)
 });
 
 
---
+++ test/contracts.spec.js
@@ -0,0 +1,24 @@
+const request = require('supertest');
+const app = require("../src/app");
+
+const { client1, contractor1, contract9, contract1 } = require('./data');
+const inValidUserId = 111111;
+
+
+
+describe('/contracts suite', () => {
+    it('auth must not works for invalid user', async() => {
+        const response = await request(app)
+            .get(`/contracts/${contract9.id}`)
+            .set({ 'profile_id': inValidUserId, Accept: 'application/json' });
+        expect(response.statusCode).toBe(401);
+    });
+    it('client 1 has one active contract', async() => {
+        const response = await request(app)
+            .get(`/contracts`)
+            .set({ 'profile_id': client1.id, Accept: 'application/json' });
+        console.log(response.body)
+        expect(response.statusCode).toBe(200);
+        expect(response.body.length).toBe(1);
+    });
+});
