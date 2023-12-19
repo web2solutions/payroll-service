23850be76552c8f86d5fde7f539f99e7204f09e1
José Eduardo Almeida <web2solucoes@gmail.com>
2023-12-19 14:47:18 -0300

    feat(/jobs/:job_id/pay): pay for jobs

--- src/app.js
+++ src/app.js
@@ -7,6 +7,60 @@ app.use(bodyParser.json());
 app.set('sequelize', sequelize)
 app.set('models', sequelize.models)
 
+let lockedClients = {};
+let lockedContractors = {};
+let lockedJobs = {};
+
+function lockClient (id) {
+    if(!lockedClients[`${id}`]) {
+        lockedClients[`${id}`] = true;
+    }
+}
+
+function unLockClient (id) {
+    if(lockedClients[`${id}`]) {
+        delete lockedClients[`${id}`]
+    }
+}
+
+function isClientLocked (id) {
+    return !!lockedClients[`${id}`];
+}
+
+// -- 
+function lockContractor (id) {
+    if(!lockedContractors[`${id}`]) {
+        lockedContractors[`${id}`] = true;
+    }
+}
+
+function unLockContractor (id) {
+    if(lockedContractors[`${id}`]) {
+        delete lockedContractors[`${id}`]
+    }
+}
+
+function isContractorLocked (id) {
+    return !!lockedContractors[`${id}`];
+}
+
+// --
+
+function lockJob (id) {
+    if(!lockedJobs[`${id}`]) {
+        lockedJobs[`${id}`] = true;
+    }
+}
+
+function unLockJob (id) {
+    if(lockedJobs[`${id}`]) {
+        delete lockedJobs[`${id}`]
+    }
+}
+
+function isJobLocked (id) {
+    return !!lockedJobs[`${id}`];
+}
 
 app.get('/contracts/:id', getProfile ,async (req, res) =>{
     const {Contract} = req.app.get('models')
@@ -59,6 +113,85 @@ app.get('/jobs/unpaid', getProfile ,async (req, res) =>{
     const jobs = await Job.findAll(queryJob)
     
     res.json(jobs)
+});
+
+
+// Pay for a job, a client can only pay if his balance >= the amount to pay. The amount should be moved from the client's balance to the contractor balance.
+app.get('/jobs/:job_id/pay', getProfile ,async (req, res) =>{
+    const { job_id } = req.params;
+    let error = null;
+
+    //
+    if(isJobLocked(job_id)) {
+        return res.status(401).json({ error: 'job locked'})
+    }
+
+    // get job
+    const {Job} = req.app.get('models')
+    const job = await Job.findOne({ where: {  id: job_id } });
+    // check job
+    if(!job) {
+        return res.status(404).json({ error: 'job not found'})
+    }
+    if(job.paid) {
+        return res.status(401).json({ error: 'job is already paid'})
+    }
+   
+    // get contract
+    const {Contract} = req.app.get('models')
+    const contract = await Contract.findOne({ where: { id: job.ContractId, ContractorId: req.profile.id } })
+    if(!contract) {
+        return res.status(404).json({ error: 'job not found'});
+    }
+    // check contract status
+    if(contract.status !== 'in_progress') {
+        return res.status(401).json({ error: 'can not pay for a inactive contract'});
+    }
+    
+    // get client
+    const {Profile} = req.app.get('models')
+    const client = await Profile.findOne({ where: { id: req.profile.id } })
+    // check client
+    if(!client) {
+        return res.status(404).json({ error: 'client not found'});
+    }
+    if(client.balance < job.price) {
+        return res.status(401).json({ error: 'insufficient funds'});
+    }
+    
+    // get contractor
+    const contractor = await Profile.findOne({ where: { id:  contract.ContractorId } }) // contract.ContractorId
+    if(!contractor) {
+        return res.status(404).json({ error: 'contractor not found'});
+    }
+    
+    try {
+        lockJob(job_id);
+        lockClient(client.id);
+        lockContractor(contractor.id)
+
+        const result = await sequelize.transaction(async (t) => {
+            const clientDeductedBalance = client.balance - job.price;
+            const contractorIncreasedBalance = contractor.balance + job.price;
+        
+            await client.update({ balance: clientDeductedBalance }, { transaction: t })
+            await contractor.update({ balance: contractorIncreasedBalance }, { transaction: t })
+
+            return { client, contractor };
+        });
+    } catch (err) {
+        error = err;
+    } finally {
+        unLockJob(job_id);
+        unLockClient(client.id);
+        unLockContractor(contractor.id)
+    }
+
+    if(error) {
+        return res.statusCode(500).json({ error })
+    } 
+
+    res.json({ data: 'paid', error })
 });
 
 
--- test/data.js
+++ test/data.js
@@ -7,12 +7,60 @@ const data = {
         balance: 1150,
         type:'client'
     },
+    client2: {
+        id: 2,
+        firstName: 'Mr',
+        lastName: 'Robot',
+        profession: 'Hacker',
+        balance: 231.11,
+        type:'client'
+    },
+    client4: {
+        id: 4,
+        firstName: 'Ash',
+        lastName: 'Kethcum',
+        profession: 'Pokemon master',
+        balance: 1.3,
+        type:'client'
+    },
     contractor1: {
         id: 5,
         firstName: 'John',
         lastName: 'Lenon',
         profession: 'Musician',
         balance: 64,
+        type:'contractor'
+    },
+    contractor5: {
+        id: 5,
+        firstName: 'John',
+        lastName: 'Lenon',
+        profession: 'Musician',
+        balance: 64,
+        type:'contractor'
+    },
+    contractor8: {
+        id: 8,
+        firstName: 'Aragorn',
+        lastName: 'II Elessar Telcontarvalds',
+        profession: 'Fighter',
+        balance: 314,
+        type:'contractor'
+    },
+    contractor7: {
+        id: 7,
+        firstName: 'Alan',
+        lastName: 'Turing',
+        profession: 'Programmer',
+        balance: 22,
+        type:'contractor'
+      },
+    contractor6: {
+        id: 6,
+        firstName: 'Linus',
+        lastName: 'Torvalds',
+        profession: 'Programmer',
+        balance: 1214,
         type:'contractor'
     },
     contract1: {
