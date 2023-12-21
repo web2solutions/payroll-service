63741b096896336fe41715f2376832181a407f18
José Eduardo Almeida <web2solucoes@gmail.com>
2023-12-21 20:15:40 -0300

    feat(express): plug CORS

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const {
   sequelize
@@ -8,6 +9,7 @@ const routes = require('./routes');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 app.set('sequelize', sequelize);
 app.set('models', sequelize.models);
 
