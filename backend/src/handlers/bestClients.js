b784d6e79ee14a467b35d57f46f1d7817db979de
José Eduardo Almeida <web2solucoes@gmail.com>
2023-12-21 20:19:02 -0300

    refactor(bestClients): best clients improvements

--- backend/src/handlers/bestClients.js
+++ backend/src/handlers/bestClients.js
@@ -3,74 +3,92 @@ const {
   Op
 } = require('../model');
 const bestClientsHandler = async (req, res) => {
-  const {
-    start,
-    end,
-    limit = 2,
-  } = req.query;
-  const {
-    Job,
-    Contract,
-    Profile
-  } = req.app.get('models');
-  const query = {
-    where: {
-      paid: true
+  try {
+    const {
+      start,
+      end,
+      limit = 2,
+    } = req.query;
+    const {
+      Job,
+      Contract,
+      Profile
+    } = req.app.get('models');
+    const query = {
+      where: {
+        paid: true
+      }
+    };
+    // console.log(start, end);
+    if (start && end) {
+      // console.log(start, end);
+      const startDate = new Date(start);
+      const endDate = new Date(end);
+      if (startDate.getTime() >= endDate.getTime()) {
+        return res.status(401).json({
+          error: 'please provide a valid date range'
+        });
+      }
+      query.where.paymentDate = query.where.paymentDate || {};
+      query.where.paymentDate[Op.between] = [startDate, endDate];
     }
-  };
-  if (start && end) {
-    const startDate = new Date(start);
-    const endDate = new Date(end);
-    if (startDate.getTime() >= endDate.getTime()) {
-      return res.status(401).json({
-        error: 'please provide a valid date range'
-      });
+    const amountPerContract = new Map();
+    const jobs = await Job.findAll(query);
+    for (const job of jobs) {
+      const totalAmount = (amountPerContract.get(job.ContractId) || 0) + job.price;
+      amountPerContract.set(job.ContractId, totalAmount);
     }
-    query.where.paymentDate = query.where.paymentDate || {};
-    query.where.paymentDate[Op.between] = [startDate, endDate];
+    
+    const sortedContracts = [...amountPerContract.entries()].sort(function (a, b) {
+      if(a[1] > b[1]) return -1;
+      if(a[1] < b[1]) return 1;
+      return 0;
+    });
+    
+    // console.log(sortedContracts);
+    const contractIds = [];
+    for(const contractInfo of sortedContracts) {
+      const [contractId/*, amount*/] = contractInfo;
+      contractIds.push(contractId);
+      // console.log(`${contractIds}  - ${limit}`)
+    }
+  
+    const contracts = await Contract.findAll({
+      where: {
+        id: contractIds,
+        // status: 'in_progress',
+      }
+    });
+    // console.log(JSON.parse(JSON.stringify(contracts)));
+    
+    const bestClientsId = [];
+    for(const id of contractIds) {
+      let contract = null;
+      for(const foundContract of contracts) {
+        if(+foundContract.id === +id) {
+          contract = foundContract;
+          break;
+        }
+      }
+      bestClientsId.push(contract.ClientId);
+    }
+  
+    const clients = await Profile.findAll({
+      where: {
+        id: bestClientsId
+      },
+      limit
+    });
+
+    // console.log(JSON.parse(JSON.stringify(clients)));
+  
+    res.json({
+      data: clients
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error });
   }
-  const amountPerContract = new Map();
-  const jobs = await Job.findAll(query);
-  for (const job of jobs) {
-    const totalAmount = (amountPerContract.get(job.ContractId) || 0) + job.price;
-    amountPerContract.set(job.ContractId, totalAmount);
-  }
-  
-  const sortedContracts = [...amountPerContract.entries()].sort(function (a, b) {
-    if(a[1] > b[1]) return -1;
-    if(a[1] < b[1]) return 1;
-    return 0;
-  });
-  
-  
-  const contractIds = [];
-  let added = 0;
-  for(const contractInfo of sortedContracts) {
-    const [contractId/*, amount*/] = contractInfo;
-    contractIds.push(contractId);
-    added += 1;
-    if(added >= limit) {
-      break;
-    }
-  }
-
-  const contracts = await Contract.findAll({
-    where: {
-      id: contractIds
-    }
-  });
-
-  
-
-  const clients = await Profile.findAll({
-    where: {
-      id: contracts.map(c => c.ClientId)
-    }
-  });
-
-  res.json({
-    data: clients
-  });
 };
 
 module.exports = bestClientsHandler;
