4af1547379ff59af0896100073c07565da57f589
José Eduardo Almeida <web2solucoes@gmail.com>
2023-12-26 16:38:04 -0300

    feat(jobsUnpaidHandler): add error handler

--- backend/src/handlers/jobsUnpaid.js
+++ backend/src/handlers/jobsUnpaid.js
@@ -1,32 +1,28 @@
 const jobsUnpaidHandler = async (req, res) => {
-  const {
-    Contract
-  } = req.app.get('models');
-  const query = {
-    where: {
-      status: 'in_progress'
+  try {
+    const { Contract, Job } = req.app.get('models');
+    const query = { where: { status: 'in_progress'  } };
+  
+    if (req.profile.type === 'client') {
+      query.where.ClientId = req.profile.id;
+    } else if (req.profile.type === 'contractor') {
+      query.where.ContractorId = req.profile.id;
     }
-  };
+  
+    const contracts = await Contract.findAll(query);
 
-  if (req.profile.type === 'client') {
-    query.where.ClientId = req.profile.id;
-  } else if (req.profile.type === 'contractor') {
-    query.where.ContractorId = req.profile.id;
+    const queryJob = {
+      where: {
+        ContractId: contracts.map(c => c.id),
+        paid: null
+      }
+    };
+    const jobs = await Job.findAll(queryJob);
+  
+    res.json(jobs);
+  } catch (error) {
+    res.status(500).json({ error });
   }
-
-  const contracts = await Contract.findAll(query);
-  const {
-    Job
-  } = req.app.get('models');
-  const queryJob = {
-    where: {
-      ContractId: contracts.map(c => c.id),
-      paid: null
-    }
-  };
-  const jobs = await Job.findAll(queryJob);
-
-  res.json(jobs);
 };
 
 module.exports = jobsUnpaidHandler;
