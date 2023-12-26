b42696f7945ad3b557597e682221d18c0df432d6
José Eduardo Almeida <web2solucoes@gmail.com>
2023-12-26 19:27:24 -0300

    refactor(bestProfession): better error handler, queries improvements

--- backend/src/handlers/bestProfession.js
+++ backend/src/handlers/bestProfession.js
@@ -1,78 +1,58 @@
 
+const Sequelize = require('sequelize');
 const {
   Op
 } = require('../model');
+
+const httpStatusCodeBasedOnMessage = (message) => {
+  let statusCode = 500;
+  if(message === 'There is no jobs in the given date range') statusCode = 404;
+  if(message === 'There is no contract in the given date range') statusCode = 404;
+  if(message === 'There is no contractor in the given date range') statusCode = 404;
+  if(message === 'please provide a valid date range') statusCode = 400;
+  return statusCode;
+};
+
 const bestProfessionHandler = async (req, res) => {
-  const {
-    start,
-    end
-  } = req.query;
-  const {
-    Job,
-    Contract,
-    Profile
-  } = req.app.get('models');
-  const query = {
-    where: {
-      paid: true
+  try {
+    const { start, end } = req.query;
+    const { Job, Contract, Profile } = req.app.get('models');
+    const query = { where: { paid: true } };
+    
+    if (start && end) {
+      const startDate = new Date(start);
+      const endDate = new Date(end);
+      if (startDate.getTime() >= endDate.getTime()) throw new Error('please provide a valid date range');
+      query.where.paymentDate = query.where.paymentDate || {};
+      query.where.paymentDate[Op.between] = [startDate, endDate];
     }
-  };
-  if (start && end) {
-    const startDate = new Date(start);
-    const endDate = new Date(end);
-    if (startDate.getTime() >= endDate.getTime()) {
-      return res.status(401).json({
-        error: 'please provide a valid date range'
-      });
-    }
-    query.where.paymentDate = query.where.paymentDate || {};
-    query.where.paymentDate[Op.between] = [startDate, endDate];
-  }
-  const amountPerContract = new Map();
-  const jobs = await Job.findAll(query);
-  for (const job of jobs) {
-    const totalAmount = (amountPerContract.get(job.ContractId) || 0) + job.price;
-    amountPerContract.set(job.ContractId, totalAmount);
-  }
 
-  let maxAmount = 0;
-  let contractIdMaxAmount = null;
-  for (const [key, value] of amountPerContract) {
-    if (value > maxAmount) {
-      maxAmount = value;
-      contractIdMaxAmount = key;
-    }
-  }
-  if(!contractIdMaxAmount) {
-    return res.json({
-      data: 'There is no jobs in the given date range'
+    const aggregatedJobsPrice = await Job.findAll({
+      where: query.where,
+      attributes: ['ContractId', [Sequelize.fn('sum', Sequelize.col('price')), 'total']],
+      group : ['Job.ContractId'],
+      raw: true,
+      order: Sequelize.literal('total DESC')
     });
-  }
-  const contract = await Contract.findOne({
-    where: {
-      id: contractIdMaxAmount
-    }
-  });
-  if(!contract) {
-    return res.json({
-      data: 'There is no contract in the given date range'
+    if(!aggregatedJobsPrice[0]) throw new Error('There is no jobs in the given date range');
+    
+    const contract = await Contract.findOne({
+      where: {
+        id: aggregatedJobsPrice[0].ContractId
+      },
+      include: [ { model: Profile, as: 'Contractor' } ]
     });
-  }
-  const contractor = await Profile.findOne({
-    where: {
-      id: contract.ContractorId
-    }
-  });
+    // if(!contract) throw new Error('There is no contract in the given date range');
+    // if(!contract.Contractor) throw new Error('There is no contractor in the given date range');
 
-  if(!contract) {
     return res.json({
-      data: 'There is no contractor in the given date range'
+      data: contract.Contractor.profession
     });
+  } catch (error) {
+    console.log(error);
+    const statusCode = httpStatusCodeBasedOnMessage(error.message);
+    return res.status(statusCode).json({ error: error.message });
   }
-
-  res.json({
-    data: contractor.profession
-  });
 };
 
 module.exports = bestProfessionHandler;
--- backend/test/integration/admin.best.profession.spec.js
+++ backend/test/integration/admin.best.profession.spec.js
@@ -1,7 +1,7 @@
 /*global  describe, it, expect */
 const request = require('supertest');
 const app = require('../../src/app');
-const { client1 } = require('./data');
+const { client1, client2 } = require('./data');
 
 // Returns the profession that earned the most money (sum of jobs paid) for any contactor that worked in the query time range.
 describe('/admin/best-profession?start=<date>&end=<date> suite', () => {
@@ -18,17 +18,42 @@ describe('/admin/best-profession?start=<date>&end=<date> suite', () => {
     const response = await request(app)
       .get(`/admin/best-profession?start=${d}&end=${d}`)
       .set({ 'profile_id': client1.id, Accept: 'application/json' });
-    expect(response.statusCode).toBe(401);
+    expect(response.statusCode).toBe(400);
     expect(response.body.error).toBe('please provide a valid date range');
   });
 
-    
-  it('programmer is the best profession between 2020-08-14 and 2020-08-15', async() => {
-    const start = '2020-08-14T00:00:00';
-    const end = '2020-08-15T00:00:00';
+
+  it('should not have found jobs between 2023-08-14 and 2023-08-15', async() => {
+    const start = '2023-08-14T00:00:00';
+    const end = '2023-08-18T00:00:00';
     const response = await request(app)
       .get(`/admin/best-profession?start=${start}&end=${end}`)
-      .set({ 'profile_id': client1.id, Accept: 'application/json' });
+      .set({ 'profile_id': client2.id, Accept: 'application/json' });
+    console.log(response.body);
+    expect(response.statusCode).toBe(404);
+    expect(response.body.error).toBe('There is no jobs in the given date range');
+  });
+
+
+  it('should not have found jobs between 2023-08-14 and 2023-08-15', async() => {
+    const start = '2023-08-14T00:00:00';
+    const end = '2023-08-18T00:00:00';
+    const response = await request(app)
+      .get(`/admin/best-profession?start=${start}&end=${end}`)
+      .set({ 'profile_id': client2.id, Accept: 'application/json' });
+    console.log(response.body);
+    expect(response.statusCode).toBe(404);
+    expect(response.body.error).toBe('There is no jobs in the given date range');
+  });
+
+    
+  fit('programmer is the best profession between 2020-08-14 and 2020-08-15', async() => {
+    const start = '2020-08-14T00:00:00';
+    const end = '2020-08-18T00:00:00';
+    const response = await request(app)
+      .get(`/admin/best-profession?start=${start}&end=${end}`)
+      .set({ 'profile_id': client2.id, Accept: 'application/json' });
+    console.log(response.body);
     expect(response.statusCode).toBe(200);
     expect(response.body.data).toBe('Programmer');
   });
